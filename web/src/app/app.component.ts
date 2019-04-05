745ae92ff029dfe8fd2a47618c7fcd9ca1aa3e63
Eddie Jaoude <eddie@jaoudestudios.com>
2019-04-05 19:18:02 +0100

    feat(version): #15 Display build version number

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TribeDashboard';
+  public title = 'TribeDashboard';
+  public version: string;
+
+  constructor() {
+    this.version = environment.version;
+}
 }
