7ce9b302aa897447c03c0f5c6cdd5d63f25307d4
venkatesh <venkatesh@squashapps.com>
2019-04-08 16:23:23 +0530

    feat(general): #10 worked on the review comments

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './app.material-module';
-
 
 @NgModule({
   declarations: [
