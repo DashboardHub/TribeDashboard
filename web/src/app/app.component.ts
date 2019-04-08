910bf8680714b37ef8d43f3bf001d03b4f02d82f
venkatesh <venkatesh@squashapps.com>
2019-04-08 17:50:08 +0530

    feat(general): #10 Worked on the indentation and spacing issues

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -12,5 +12,5 @@ export class AppComponent {
 
   constructor() {
     this.version = environment.version;
-}
+  }
 }
--- web/src/app/app.material-module.ts
+++ web/src/app/app.material-module.ts
@@ -1,15 +1,15 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-    declarations: [],
-    exports: [
-          BrowserAnimationsModule,
-          MatCardModule,
-          MatButtonModule
-    ],
-    providers: [],
-    bootstrap: []
-  })
-  export class AppMaterialModule { }
+  declarations: [],
+  exports: [
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [],
+})
+export class AppMaterialModule { }
