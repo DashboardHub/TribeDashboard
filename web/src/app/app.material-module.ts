3c085375ffc8e18a783caeb3f9b3aa23281e5986
venkatesh <venkatesh@squashapps.com>
2019-04-08 15:57:33 +0530

    feat(general): #10 Created generic module for angular material

---
+++ web/src/app/app.material-module.ts
@@ -0,0 +1,14 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+@NgModule({
+    declarations: [
+     
+    ],
+    imports: [
+    
+      BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: []
+  })
+  export class AppMaterialModule { }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app.material-module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
