7c7016017c812e681b12292fb60704a726196b2b
venkatesh <venkatesh@squashapps.com>
2019-04-09 10:42:45 +0530

    feat(design): #18 created routing for the login page

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [LoginPageComponent]
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './app.material-module';
 import { LoginPageComponent } from './components/login-page/login-page.component';
@@ -8,7 +8,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent,
+    routingComponent
   ],
   imports: [
     BrowserModule,
