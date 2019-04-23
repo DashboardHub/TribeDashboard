2f9a3a8860bd3a5c30b8749f75420ceca227bdda
Venkatesh <venkatesh@squashapps.com>
2019-04-23 19:35:41 +0530

    feat(connect accounts): #62 Modified user model and added functionality to store secondary user

--- web/src/app/components/social-cards/social-cards.component.ts
+++ web/src/app/components/social-cards/social-cards.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UserSocial } from 'src/app/models/userSocial.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-social-cards',
@@ -9,9 +12,50 @@ import { UserSocial } from 'src/app/models/userSocial.model';
 export class SocialCardsComponent implements OnInit {
 
   @Input() userSocial: UserSocial;
+  private isPrimary: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.isPrimary = false;
+  }
+
+  connectAccount(provider: string) {
+    switch (provider) {
+      case 'github':
+        this.addGithubAccount();
+        break;
+      case 'twitter':
+        this.addTwitterAccount();
+        break;
+      default:
+        break;
+    }
+  }
+
+  addGithubAccount() {
+    this.authService.signInWithGithub(this.isPrimary)
+      .subscribe((response) => {
+        this.saveSecondaryUser(response);
+      });
+  }
+
+  addTwitterAccount() {
+    this.authService.signInWithTwitter(this.isPrimary)
+      .subscribe((response) => {
+        this.saveSecondaryUser(response);
+      });
+  }
+
+  saveSecondaryUser(user: User) {
+    this.userService.saveUser(user)
+      .subscribe((response) => {
+        // TODO: store secondary user social and stats after successful storage of user details.
+        console.log('response after saving in firebase', response);
+      });
+  }
 
 }
--- web/src/app/models/user.model.ts
+++ web/src/app/models/user.model.ts
@@ -1,15 +1,19 @@
+import { SocialConnections } from './socialConnections';
+
 export interface User {
   displayName: string;
   photoUrl: string;
   uid: string;
   creationAt: string;
   lastSignInAt: string;
+  isPrimary: boolean;
   credentials: {
     accessToken: string,
     provider: string,
     refreshToken?: string,
     secret?: string,  // Optional field
   };
+  socialConnections?: SocialConnections;
   additionalUserInfo: {
     profile: {
       createdAt: string,
